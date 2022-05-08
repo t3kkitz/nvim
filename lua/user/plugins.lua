03a1edd343d7b5b7ecd41677f70e2f098c5d2544
Sergei @t3kkitz Bulgakov <t3kkitz@gmail.com>
2022-05-08 17:55:32 +0400

    add nvim-tree

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,6 +46,13 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   --  use 'ggandor/leap.nvim' -- EasyMotion text navigation plugin
   -- require('leap').set_default_keymaps()
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 
   -- Colorschemes
   use 'LunarVim/Colorschemes'
