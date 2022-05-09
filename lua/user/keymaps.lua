10bb9ac3eb03089394a050695facbc57743637ab
Sergei @t3kkitz Bulgakov <t3kkitz@gmail.com>
2022-05-09 15:19:39 +0400

    add null-ls

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -69,3 +69,6 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- Nvimtree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+
+-- Formatting
+keymap("n", "<leader>f", ":Format<cr>", opts)
